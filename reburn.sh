e7c8350f1f28a4139634982b63cf0a1984d07cd4
ndf-zz <ndf-zz@6-v.org>
2025-09-04 18:29:34 +1000

    fix path typo in reburn script

--- reburn.sh
+++ reburn.sh
@@ -17,5 +17,5 @@ fi
 
 esptool.py --before default_reset --after no_reset --chip esp32 erase_flash --force
 idf.py bootloader
-esptool.py --chip esp32 --before=default_reset --after=no_reset write_flash --flash_mode dio --flash_freq 40m --flash_size 4MB 0x0 /home/ndf/esp/bleuart/build/bootloader/bootloader-reflash-digest.bin --force
+esptool.py --chip esp32 --before=default_reset --after=no_reset write_flash --flash_mode dio --flash_freq 40m --flash_size 4MB 0x0 build/bootloader/bootloader-reflash-digest.bin --force
 idf.py flash monitor
